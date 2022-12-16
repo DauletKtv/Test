6e473b6b84ba5e434f2bcd329ff3a6c0cb6cdb73
Eraly <81900112+Chambo015@users.noreply.github.com>
2022-12-16 21:33:23 +0600

    era

---
+++ src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        {{name}}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return { 
+            name: 'Tedawdaxt'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
